b1dd1eabee9d67ddbf209e7352f6852bcfa398d5
chaser <epochchaser@gmail.com>
2018-08-31 16:01:22 +0900

    demux base setting

---
+++ src/ObjectActionHandler.js
@@ -0,0 +1,24 @@
+const { AbstractActionHandler } = require('demux')
+
+const state = {
+  volumeBySymbol: {},
+  totalTransfers: 0,
+  indexState: { blockNumber: 0, blockHash: '' }
+} // Initial state
+
+class ObjectActionHandler extends AbstractActionHandler {
+  async handleWithState(handle) {
+    await handle(state)
+  }
+
+  async loadIndexState() {
+    return state.indexState
+  }
+
+  async updateIndexState(stateObj, block) {
+    stateObj.indexState.blockNumber = block.blockInfo.blockNumber
+    stateObj.indexState.blockHash = block.blockInfo.blockHash
+  }
+}
+
+module.exports = ObjectActionHandler
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { BaseActionWatcher } = require('demux')
+const { NodeosActionReader } = require('demux-eos') // eslint-disable-line
+const ObjectActionHandler = require('./ObjectActionHandler')
+const updaters = require('./updaters')
+const effects = require('./effects')
+
+const actionHandler = new ObjectActionHandler(updaters, effects)
+
+const actionReader = new NodeosActionReader(
+  'http://mainnet.eoscalgary.io', // Thanks EOS Calgary!
+  0 // Start at most recent blocks
+)
+
+const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 500)
+
+actionWatcher.watch()
