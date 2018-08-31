720b87f822d8ef6b99a012a3f40ae5b49c4f169d
chaser <epochchaser@gmail.com>
2018-08-31 16:33:46 +0900

    nodeos config

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ const effects = require('./effects')
 
 const actionHandler = new ObjectActionHandler(updaters, effects)
 
-const actionReader = new NodeosActionReader(
-  'https://eos.greymass.com:443',
-  0 // Start at most recent blocks
-)
+const actionReader = new NodeosActionReader('https://eos.greymass.com:443', 13962005, false)
 
-const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 500)
+const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 250)
 
 actionWatcher.watch()
